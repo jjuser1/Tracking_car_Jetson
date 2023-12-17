eee4c336f90efbe1d3a264a12498bc07691ea18a
jjsuer1 <ajsirhzz75@konkuk.ac.kr>
2023-12-17 12:01:50 +0000

    version1

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+#-*-coding: utf-8-*-
+def run_program():
+    try:
+        subprocess.run(['python', 'code/frame_extract.py'])
+    except FileNotFoundError:
+        print("Unable to find the file 'frame_extract.py'.")
+    try:
+        subprocess.run(['python', 'code/draw_area.py'])
+    except FileNotFoundError:
+        print("Unable to find the file 'draw_area.py'.")
+    try:
+        subprocess.run(['python', 'code/videotest.py'])
+    except FileNotFoundError:
+        print("Unable to find the file 'videotest.py'.")
+
+run_program()
